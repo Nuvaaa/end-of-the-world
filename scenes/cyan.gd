84a38c6d0cfc9c73a445ee2c08c1bd1ee71afcaa
Nuva <adifronzo71@gmail.com>
2025-10-23 03:15:32 -0500

    NEW CODE

---
+++ scenes/cyan.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var moveDirection = Vector2(0, 0)
+
+func walk():
+	moveDirection[0] = Input.get_axis("left", "right")
+
+func friction():
+	var fric : float
+	var tile = $"../Ground".get_cell_tile_data($"../Ground".local_to_map(position + Vector2(0, 15)))
+	if tile:
+		fric = tile.get_custom_data("friction")
+	else:
+		fric = 0.1
+	
+func _process(_delta):
+	walk()
+	friction()
+	
+	move_and_slide()
--- scripts/camera_2d.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-
-func _on_camera(x1, x2, y1, y2):
-	limit_left = x1
-	limit_right = x2
-	limit_top = y1
-	limit_bottom = y2
